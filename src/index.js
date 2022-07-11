daef47961500378019096565fdaf0dc13c833477
Max Meischke <meischkem@outlook.com>
2022-07-11 11:25:59 -0400

    removed most test code

--- src/index.js
+++ src/index.js
@@ -2,38 +2,17 @@ import './style.css';
 import createPokemonList from './pokemonList';
 
 
-// Calling the API and displaying to test that it works
-const pokeList = document.querySelector('.pokemon-list');
-
-let pokedex = 'kanto';
-
 // Query the correct pokedex and return
-async function getPokedex(dex){
+export default async function getPokedex(dex){
     const response = await fetch(`https://pokeapi.co/api/v2/pokedex/${dex}/`);
     const dexReturned = await response.json();
     return dexReturned;
 };
 
-/* // Display the list of pokemon in the dex to confirm it is working
-function pokemonList(dexData){
-    let pokedexList = dexData.pokemon_entries;
-    for(let i=0;i<pokedexList.length;i++){
-        let pokemonName = pokedexList[i].pokemon_species.name;
-        pokeList.innerHTML += `${pokemonName} <br>`
-    };
-};
-
-async function runPokeDex(dex){
-    const getDex = await getPokedex(dex);
-    pokemonList(getDex);
-} */
 
 //runPokeDex(pokedex);
 console.log('test');
-createPokemonList(getPokedex('kanto'));
-createPokemonList(getPokedex('kanto'));
-createPokemonList(getPokedex('kanto'));
+createPokemonList('kanto');
+//createPokemonList(getPokedex('kanto'));
+//createPokemonList(getPokedex('kanto'));
 
-
-/* const testPara = document.querySelector('.test')
-testPara.innerText = 'Working'; */
