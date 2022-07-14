4d2add6b8f6da2f83b6f1b956076402e20a6ca2d
Max Meischke <meischkem@outlook.com>
2022-07-14 11:12:10 -0400

    Added evntLis - update dex based on entry clicked

--- src/pokemonList.js
+++ src/pokemonList.js
@@ -1,6 +1,7 @@
 // This file contains the code to create the Pokemon list 
 import getPokedex from './index';
 import {formatPokemonName, formatPokemonNumber} from './formatData';
+import createPokedexDisplay from './pokedexDisplay';
 
 export default async function createPokemonList(dex){
     const pokemonList = document.querySelector('.pokemon-list');
@@ -14,13 +15,14 @@ export default async function createPokemonList(dex){
 
     for(let i=0; i<=dexData.pokemon_entries.length; i++){
         // Declare var for each data point needed
+        let pokemonID = dexData.pokemon_entries[i].entry_number;
         let pokemonNum = formatPokemonNumber(dexData.pokemon_entries[i].entry_number);
         let pokemonImg = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${i+1}.png`;
         let pokemonName = formatPokemonName(dexData.pokemon_entries[i].pokemon_species.name);
-        createPokemonEntry(pokemonImg,pokemonName,pokemonNum);
+        createPokemonEntry(pokemonID,pokemonImg,pokemonName,pokemonNum);
     }
 
-    function createPokemonEntry(image, name, num){
+    function createPokemonEntry(id, image, name, num){
         // Container for entry
         const pokemonListEntry = document.createElement('div');
         pokemonListEntry.classList.add('pokemon-entry');
@@ -38,6 +40,11 @@ export default async function createPokemonList(dex){
         const pokemonListNumber = document.createElement('p');
         pokemonListNumber.classList.add('pokemon-number');
         pokemonListNumber.textContent = num;
+
+        // Adding an event listener to each entry to update dex when clicked
+        pokemonListEntry.addEventListener('click', ()=>{
+            createPokedexDisplay(id);
+        });
 
         // Create container
         pokemonList.appendChild(pokemonListEntry);
